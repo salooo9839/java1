45f44b069e5c12ebe2f5091232334ff828bfc8c2
SK CYBER <skyjan128@gmail.com>
2026-01-28 18:12:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Step 3: Display "Hello World"
+
+// Show "Hello World" using alert
+alert("Hello World");
+
+// Show "Hello World" in the browser console
+console.log("Hello World");
+
+
+// Step 4: Use Variables (var, let, const)
+
+// Using var
+var studentName = "salman khan";
+
+// Using let
+let courseName = "JavaScript Basics";
+
+// Using const
+const batchNumber = 1;
+
+// Display the variables in the console
+console.log("Student Name:", studentName);
+console.log("Course Name:", courseName);
+console.log("Batch Number:", batchNumber);
